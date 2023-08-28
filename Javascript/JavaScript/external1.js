53b47814ff8388d02c43dd32979775feacf88d36
M H Harsha Ramakrishna <98935743+Learnerforevr@users.noreply.github.com>
2023-08-28 14:26:49 +0530

    Add files via upload

---
+++ Javascript/JavaScript/external1.js
@@ -0,0 +1,9 @@
+var n='eswari';
+document.write(n);
+n =' likitha&navya';
+document.write(n);
+document.write("<br>");
+document.write(n.length);
+document.write("Enter a number");
+console.log(10);
+alert(10);
